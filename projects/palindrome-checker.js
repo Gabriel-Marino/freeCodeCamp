00d1db9dfe3f8553985aa788ff9f7b9686cb2274
Gabriel-Marino <gcmarino404@gmail.com>
2022-07-02 19:21:16 -0300

    Revert "init repo"
    This reverts commit 140453d69c45e8265d1d350ac51ca3e0328382c8.

--- projects/palindrome-checker.js
+++
@@ -1,8 +0,0 @@
-function palindrome(word) {
-  const LOWERCASEWORD = word.toLowerCase();
-  const ONLYALPHAWORD = LOWERCASEWORD.replace(/[^a-z0-9]/gi, '');
-
-  const REVERSEDWORD = ONLYALPHAWORD.split("").reverse().join("");
-
-  return ONLYALPHAWORD === REVERSEDWORD;
-}
